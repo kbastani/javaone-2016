f8caf49ad278737871acfb07829dd43ca44bf443
Chris Richardson <chris@chrisrichardson.net>
2016-09-21 14:31:56 -0700

    Fixed bogus test scope dependency, misc cleanups including build-and-test-all.sh script

---
+++ microservices/build-and-test-all.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+set -e
+
+. ./set-env.sh
+
+
+docker-compose -f docker-compose-eventuate-local.yml -f docker-compose.yml stop
+docker-compose -f docker-compose-eventuate-local.yml -f docker-compose.yml rm -v --force
+
+docker-compose -f docker-compose-eventuate-local.yml up -d
+
+mvn package
+
+docker-compose -f docker-compose-eventuate-local.yml -f docker-compose.yml up -d
+
+./wait-for-services.sh $DOCKER_HOST_IP 8080
+
+echo testing. look for successful message
+
+./e2e-test.sh $DOCKER_HOST_IP
+
+echo SUCCESS
