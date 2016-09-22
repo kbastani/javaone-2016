393ed65acafdb5948406714ec8075b55af981e5d
Chris Richardson <chris@chrisrichardson.net>
2016-09-21 20:27:31 -0700

    Cleaned up dependencies

--- microservices/build-and-test-all.sh
+++ microservices/build-and-test-all.sh
@@ -12,6 +12,8 @@ docker-compose -f docker-compose-eventuate-local.yml up -d
 
 mvn package
 
+docker-compose -f docker-compose-eventuate-local.yml -f docker-compose.yml build
+
 docker-compose -f docker-compose-eventuate-local.yml -f docker-compose.yml up -d
 
 ./wait-for-services.sh $DOCKER_HOST_IP 8080
